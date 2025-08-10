b021edbce23b129e3ccbc103342a253dd2d0610c
liushijie <liushijie01@inspur.com>
2025-08-10 15:49:22 +0800

    cleanup: remove temporary @roo-code/cloud shim; rely on npm package types; ensure check-types passes

--- src/types/shims/roo-code-cloud.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@roo-code/cloud" {
-	export const CloudService: any
-	export function getRooCodeApiUrl(...args: any[]): any
-	export function getClerkBaseUrl(...args: any[]): any
-	export const PRODUCTION_CLERK_BASE_URL: string
-}
